95b8c0450c02885d9f9b41859b5bb639e1c8b183
1996yyk <1996yyk@gmail.com>
2020-08-26 00:45:41 +0900

    Button Class
    버튼 클래스 생성 및 데이터 전송

--- Protocol/Protocol.ino
+++ Protocol/Protocol.ino
@@ -1,28 +1,32 @@
 #include <Wire.h>
 #include <Servo.h>
+#include <stdint.h>
+
+const int butPin1 = 6;
+const int butPin2 = 7;
+const int MPU=0x68;
 
 class Flex{
   private:
-    uint16_t value;
+    uint16_t value[5];
   public:
     void Receive(); // FlexSensor -> Arduino
     void Send(); // Arduino -> Unity
 
   Flex(){
-    uint16_t value[5] = { 0, 0, 0, 0, 0 };
   }
 
 };
 
-void Flex::Receive()
+void Flex::Receive() // FlexSensor -> Arduino
 {
   for(int i = 0 ; i < 5 ; i ++ )
   {
-    this->value[i] = analogRead(i); // Flex Sensor 값 업데이
+    this->value[i] = analogRead(i); 
   }
 }
 
-void Flex::Send()
+void Flex::Send() // Arduino -> Unity
 {
   for(int i = 0 ; i < 5 ; i++ )
   {
@@ -41,7 +45,7 @@ class Gyro{
     Gyro(){
       Wire.beginTransmission(MPU);
       Wire.write(0x3B);
-      Wire.endTransmission(flase);
+      Wire.endTransmission(false);
       Wire.requestFrom(MPU,14,true);
       AcX = 0;
       AcY = 0;
@@ -55,7 +59,7 @@ class Gyro{
     
 };
 
-void Gyro::Receive()
+void Gyro::Receive() // Gyro Sensor -> Arduino 
 {
   AcX=Wire.read()<<8|Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)    
   AcY=Wire.read()<<8|Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
@@ -65,12 +69,12 @@ void Gyro::Receive()
   GyY=Wire.read()<<8|Wire.read();  // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
   GyZ=Wire.read()<<8|Wire.read();  // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
 }
-void Gyro::Send()
+void Gyro::Send() //Arduino -> Unity 
 {
   Serial.write(AcX);
   Serial.write(AcY);
   Serial.write(AcZ);
-  Serial.write(Tmp/340.00+36.53);  
+  Serial.write(Tmp/340+36);  
   Serial.write(GyX);
   Serial.write(GyY);
   Serial.write(GyZ);
@@ -81,7 +85,7 @@ class gloVR_Servo{
   private:
     uint16_t deg;
   public:
-    void ch_deg();
+    void ch_deg(); // 각도 바꾸는 함수 
 
     gloVR_Servo(){
       deg = 0;
@@ -89,58 +93,90 @@ class gloVR_Servo{
     }
 };
 
-void Survo::ch_deg() // 각도변
+void gloVR_Servo::ch_deg() // 각도 바꾸는 함수 
 {
-  this->servo.write(
+  
 }
 
 class Data{
   private:
-    // 플렉스센서값, 자이로센서값, 영상처리 값
-    int
+    // 플렉스센서값, 자이로센서값, 영상처리 값 배열로 받자.
   public:
   
-}
+  
+};
 
 class SetArdToUni{
   private:
   public:
-
-  SetArdToUni(){
-    Serial.begin(9600);
-  }
+    SetArdToUni(){
+      Serial.begin(9600);
+    }
 };
 
+class But{
+  private:
+  public:
+    void Left();
+    void Right();
+    
+    But(){
+      pinMode(butPin1, INPUT);
+      pinMode(butPin2, INPUT);
+  
+      digitalWrite(butPin1,HIGH);
+      digitalWrite(butPin2,HIGH);
+    }
+};
+
+void But::Left(){
+  Serial.println("LEFT");
+  Serial.write(1);
+  Serial.flush();
+  delay(50);
+}
+
+void But::Right(){
+  Serial.println("RIGHT");
+  Serial.write(2);
+  Serial.flush();
+  delay(50);
+  
+}
+
 //플렉스센서, 자이로센서, 서보모터 객체 생성.
+/*
 Flex flex;
 Gyro gyro;
 Survo survo[5];
+*/
+
+// 버튼 객체 생성
+But but;
 
 void setup() {
   // put your setup code here, to run once:
   SetArdToUni setardtouni = SetArdToUni(); // 시리얼 통신 시작
   
   //플렉스센서, 자이로센서, 서보모터 객체들의 각 값들 초기화.
+/*
   flex = Flex();
   survo[5] = { Survo(), Survo(), Survo(), Survo(), Survo() };
   gyro = Gyro();
+*/
+
+  but = But();
   
- }
+}
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
-  /*bool option_degree;
-  int degree[5];
-
-  if(option_degree == true)
-  { 
-    for(i = 0 ; i < 5 ; i++)
-    {
-      // 옵션에서 서보모터 값 입력받기.
-      degree = Serial.read
-      survo[i].degree = 
-    } 
-  }*/
-
+  // 버튼 테스트 ( 아두이노 -> 유니티 ) 
+  if(digitalRead(butPin1) == LOW){
+    but.Left();
+  }
+  if(digitalRead(butPin2) == LOW){
+    but.Right();
+  }
+ 
 }
