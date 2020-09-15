edbbb6dcd8c40a5a5beb0249911212e09599fa3e
1996yyk <1996yyk@gmail.com>
2020-09-16 01:16:02 +0900

    Revert "Merge branch 'master' of https://github.com/YUNKWANGYOU/gloVR"
    This reverts commit a8015e39c2231f451197d7630b69820439edf2d5, reversing
    changes made to 14c7722cee5aaaaf6706e15b0bf964a180c27b15.

--- OpenCV/hand.py
+++ OpenCV/hand.py
@@ -1,21 +1,15 @@
 import cv2
 import numpy as np
 import time
-import math
-import socket
-
-UDP_IP = "127.0.0.1"
-UDP_PORT = 5065
-
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
 
 # Open Camera object
 cap = cv2.VideoCapture(0)
 
 # Decrease frame size
-cap.set(3, 1000)
-cap.set(4, 1200)
+#cap.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, 1000)
+#cap.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, 600)
+
 
 def nothing(x):
     pass
@@ -102,12 +96,8 @@ while (1):
             ci = i
 
         # Largest area contour
-    print("ci = ",ci)
-    print("type = ",type(contours),"len = ",len(contours))
     cnts = contours[ci]
-
-
-
+    
     # Find convex hull
     hull = cv2.convexHull(cnts)
 
@@ -211,13 +201,6 @@ while (1):
     k = cv2.waitKey(5) & 0xFF
     if k == 27:
         break
-
-    try:
-
-        sock.sendto((str(x)+","+str(y)).encode(), (UDP_IP, UDP_PORT) )
-        print((str(x)+","+str(y)))
-    except:
-        pass
 
 cap.release()
 cv2.destroyAllWindows()
