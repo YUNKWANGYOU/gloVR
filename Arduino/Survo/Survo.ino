14f39ddafed0684f891b20023c43715a26b158fa
kongkongee <70675824+kongkongee@users.noreply.github.com>
2020-09-23 17:28:46 +0900

    서보 설정 수정

--- Arduino/Survo/Survo.ino
+++ Arduino/Survo/Survo.ino
@@ -1,15 +1,24 @@
 #include <SoftwareSerial.h>
 #include <Servo.h>
- 
-#define BT_RXD 6
-#define BT_TXD 7
-#define ServoPin 8
 
+// 나노 기준 회로설정 사진참고
+#define BT_RXD 0
+#define BT_TXD 1
+#define Servo1 3
+#define Servo2 4
+#define Servo3 5
+#define Servo4 6
+#define Servo5 7
+
+// 블루투스, 서보 설정
 SoftwareSerial bluetooth(BT_RXD, BT_TXD);
 Servo servo1;
+Servo servo2;
+Servo servo3;
+Servo servo4;
+Servo servo5;
 
-int angle = 0;
-
+// 블루투스 수신 값
 byte byteArr[7];
 int i=0;
  
@@ -17,10 +26,19 @@ void setup(){
   Serial.begin(9600);
   bluetooth.begin(9600);
 
-  servo1.attach(ServoPin);
+  servo1.attach(Servo1);
+  servo2.attach(Servo2);
+  servo3.attach(Servo3);
+  servo4.attach(Servo4);
+  servo5.attach(Servo5);
 }
  
 void loop(){
+  // 블루투스 수신 값 한가지
+  // ? 여러가지 받을땐 형식이 어떻게?
+  // 수신형태 알려주면 수정하겠음.
+  // 수정 필요
+  // 집가서 함
   if (bluetooth.available()) {
     byte readData = bluetooth.read();
     Serial.write(readData);
