a87f71875b9c8f725d8fad3974aa8625119dee27
1996yyk <1996yyk@gmail.com>
2020-09-24 16:43:11 +0900

    통신

--- OpenCV/cToPy.py
+++ OpenCV/cToPy.py
@@ -1,12 +1,12 @@
 import socket
 
 target_host = "127.0.0.1"
-target_port = 27000
+target_port = 8000
 
 # create a socket object
 client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
+client.bind((target_host,target_port))
 # receive some data
-data, addr = client.recvfrom(27000)
+data, addr = client.recvfrom(200)
 
 print(data)
