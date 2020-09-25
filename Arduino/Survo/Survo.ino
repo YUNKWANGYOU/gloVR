43b00db41bbadf96676c54c5294a6ed7f929b7f4
kongkongee <70675824+kongkongee@users.noreply.github.com>
2020-09-25 17:43:34 +0900

    Servo 5손가락으로 수정

--- Arduino/Survo/Survo.ino
+++ Arduino/Survo/Survo.ino
@@ -20,9 +20,9 @@ Servo servo5;
 
 // 블루투스 수신 값
 byte byteArr[7];
-int i=0;
- 
-void setup(){
+int i = 0;
+
+void setup() {
   Serial.begin(9600);
   bluetooth.begin(9600);
 
@@ -32,8 +32,8 @@ void setup(){
   servo4.attach(Servo4);
   servo5.attach(Servo5);
 }
- 
-void loop(){
+
+void loop() {
   // 블루투스 수신 값 한가지
   // ? 여러가지 받을땐 형식이 어떻게?
   // 수신형태 알려주면 수정하겠음.
@@ -46,52 +46,118 @@ void loop(){
     byteArr[i++] = readData;
   }
 
-  if(i == 7){
+  if (i == 7) {
     i = 0;
     Serial.println("all done");
-    if(checkData(byteArr) == 0){
+    if (checkData(byteArr) == 0) {
       Serial.print("잘못된 데이터입니다!");
     }
-
-    if(byteArr[1] == '0'){
+///////////////
+    if (byteArr[1] == '0') {
       servo1.write(0);
     }
 
-    if(byteArr[1] == '1'){
+    if (byteArr[1] == '1') {
       servo1.write(60);
     }
-    
-    if(byteArr[1] == '2'){
+
+    if (byteArr[1] == '2') {
       servo1.write(120);
     }
-    
-    if(byteArr[1] == '3'){
+
+    if (byteArr[1] == '3') {
       servo1.write(150);
     }
-    clearArr(byteArr);
-    
-  }
-  
-  if (Serial.available()) {
-    bluetooth.write(Serial.read());
+
+    if (byteArr[2] == '0') {
+      servo2.write(0);
+    }
+///////////////
+    if (byteArr[2] == '1') {
+      servo2.write(60);
+    }
+
+    if (byteArr[2] == '2') {
+      servo2.write(120);
+    }
+
+    if (byteArr[2] == '3') {
+      servo2.write(150);
+    }
+//////////////
+    if (byteArr[3] == '0') {
+      servo3.write(0);
+    }
+
+    if (byteArr[3] == '3') {
+      servo3.write(60);
+    }
+
+    if (byteArr[3] == '2') {
+      servo3.write(120);
+    }
+
+    if (byteArr[3] == '3') {
+      servo3.write(150);
+    }
+/////////////////
+    if (byteArr[4] == '0') {
+      servo4.write(0);
+    }
+
+    if (byteArr[4] == '1') {
+      servo4.write(60);
+    }
+
+    if (byteArr[4] == '2') {
+      servo4.write(120);
+    }
+
+    if (byteArr[4] == '3') {
+      servo4.write(150);
+    }
+////////////////// 
+  if (byteArr[5] == '0') {
+    servo5.write(0);
   }
 
-  
+  if (byteArr[5] == '1') {
+    servo5.write(60);
+  }
+
+  if (byteArr[5] == '2') {
+    servo5.write(120);
+  }
+
+  if (byteArr[5] == '3') {
+    servo5.write(150);
+  }
+  clearArr(byteArr);
+
 }
 
-int checkData(byte * ar){
-  if(ar[0] != 's')
-    return 0;
 
-  if(ar[6] != 'e')
-    return 0;
 
-   return 1;
+if (Serial.available()) {
+  bluetooth.write(Serial.read());
 }
 
-int clearArr(byte * ar){
-  int k=0;
-  for(k=0;k<7;k++){
+
+}
+
+int checkData(byte * ar) {
+  if (ar[0] != 's')
+    return 0;
+
+  if (ar[6] != 'e')
+    return 0;
+
+  return 1;
+}
+
+int clearArr(byte * ar) {
+  int k = 0;
+  for (k = 0; k < 7; k++) {
     ar[k] = 0;
   }
 }
